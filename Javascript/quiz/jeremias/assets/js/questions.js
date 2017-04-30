b0ed271c38d62bacdf0ba2053668f631df9de891
Jeremias Moreira Gomes <j3r3miasmg@gmail.com>
2017-04-30 18:36:05 -0300

    Questions.

---
+++ Javascript/quiz/jeremias/assets/js/questions.js
@@ -0,0 +1,60 @@
+/**
+ * Created by jeremias on 30/04/17.
+ */
+var allQuestions = [
+    {
+        question: 'Puta falta de...',
+        choices: ['sacanagem', 'amor', 'maconha', 'baitolagem'],
+        correctAnswer: 0,
+        done: false
+    },
+    {
+        question: 'Tah saindo da jaula o...',
+        choices: ['monstro', 'gorila', 'tigre', 'leao'],
+        correctAnswer: 0,
+        done: false
+    },
+    {
+        question: 'Morre...',
+        choices: ['seu chato', 'deabo', 'seu escroto', 'fdp'],
+        correctAnswer: 1,
+        done: false
+    },
+    {
+        question: 'Cabalo imundo...',
+        choices: ['e chato', 'noob', 'do caralho', 'do diabo'],
+        correctAnswer: 2,
+        done: false
+    },
+    {
+        question: 'É um pais ...',
+        choices: ['da Europa', 'da Asia', 'da America Central', 'da Oceania'],
+        correctAnswer: 0,
+        done: false
+    },
+    {
+        question: 'Ja acabou, ...',
+        choices: ['Giovana', 'Jussara', 'Gabriella', 'Jessica'],
+        correctAnswer: 3,
+        done: false
+    },
+    {
+        question: 'Nao sou capaz...',
+        choices: ['de sobreviver', 'de saber', 'de dizer', 'de opinar'],
+        correctAnswer: 3,
+        done: false
+    },
+    {
+        question: 'Gente...',
+        choices: ['vou ter que fazer de novo', 'que que tacotesseno', 'qual a necessidade disso', 'como pode'],
+        correctAnswer: 2,
+        done: false
+    },
+    {
+        question: 'Vc estuda...',
+        choices: ['na escola', 'no brisolo', 'na rayane', 'no meu nome'],
+        correctAnswer: 2,
+        done: false
+    }
+
+];
